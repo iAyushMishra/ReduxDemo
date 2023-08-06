7a0b4febf1d390c649c102f1a9472162bc3e85df
MishraAyush <amishra.162@gmail.com>
2023-08-06 21:05:11 +0530

    Initial Commit

---
+++ ReduxDemo/ReduxDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxDemoApp.swift
+//  ReduxDemo
+//
+//  Created by Ayush Mishra on 06/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ReduxDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
